2f0866f6729fdb63d257ca381c781c557800cf27
dali2000 <medalikebair@gmail.com>
2022-03-26 22:24:14 +0100

    add

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -7,11 +7,17 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { RequestsResidenceComponent } from './requests-residence/requests-residence.component';
+import { AddResidencesComponent } from './add-residences/add-residences.component';
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
   { path: 'Register', component: RegisterComponent },
-  { path: 'Login', component: LoginComponent }
+  { path: 'Login', component: LoginComponent },
+  { path: 'Home_admin', component: AdminHomeComponent },
+  { path: 'Request_Residence', component: RequestsResidenceComponent },
+  { path: 'Add_Residence', component: AddResidencesComponent },
 ];
 
 @NgModule({
@@ -20,7 +26,10 @@ const routes: Routes = [
     WelcomeComponent,
     LoginComponent,
     RegisterComponent,
-    SidebarComponent
+    SidebarComponent,
+    AdminHomeComponent,
+    RequestsResidenceComponent,
+    AddResidencesComponent
   ],
   imports: [
     BrowserModule,
