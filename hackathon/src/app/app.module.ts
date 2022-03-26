75adb75eea47ba5bc3c66dfbd0f231d1787753e0
Ahmed Stambouli <ahmed.stambouli98@gmail.com>
2022-03-26 19:37:42 +0100

    login et register et welcom

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  { path: 'Register', component: RegisterComponent },
+  { path: 'Login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
