1c1f67926d25d78ff1ba677352700e6294cbe8e6
dali2000 <medalikebair@gmail.com>
2022-03-27 01:50:42 +0100

    relate db

--- hackathon/src/app/add-residences/add-residences.component.ts
+++ hackathon/src/app/add-residences/add-residences.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-residences',
   templateUrl: './add-residences.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddResidencesComponent implements OnInit {
 
-  constructor() { }
+  Residence = {
+    Label:null,
+    adresse:null,
+    NAppartement: null,
+    owner:null,
+    date:null,
+    id:null
+  }
+  
+
+  constructor(private http:HttpClient, private router:Router) { }
 
   ngOnInit(): void {
   }
+  addResidences(){
+    this.http.post('http://localhost:3000/api/admin/addResidence',this.Residence).subscribe(
+      res => {    
+        console.log(res)
+      });
+  }
+
 
 }
--- hackathon/src/app/login/login.component.ts
+++ hackathon/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import jwtDecode from 'jwt-decode';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http:HttpClient, private router:Router) { }
+  
+   user = {
+    email:"",
+    password:""
+  }
+  password :"";
+  email:"";
+  data:any;
+  token:any;
+  login(){
+    this.http.post("http://localhost:3000/api/auth/login",this.user).subscribe(res=>{
+      this.data = res;
+      console.log(this.data);
+      this.token = this.data.token;
+      // localStorage.setItem('token',this.token);
+    })
+    if(this.token == null){
+      this.router.navigate((['/Login']))
+  
+    }else{
+      localStorage.setItem('token',this.token);
+    
+      this.router.navigate((['/Home_admin']))
+    }
+  }
   ngOnInit(): void {
+
   }
 
 }
